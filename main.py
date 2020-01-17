b0f49d69c66714faf62789f205ef12884fa7a20f
ErnestNamHee <37908030+ErnestNamHee@users.noreply.github.com>
2020-01-17 17:15:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def hello_world():
     return 'IEF Edge Apps : Hello World !!!'  
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8100)
+    app.run(debug=True, port=8100)
